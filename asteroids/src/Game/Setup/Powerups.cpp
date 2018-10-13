a8a606cabb981c2a5b4fa6d344cb04e4e606e738
Franco <franco_vega@msn.com>
2018-10-13 03:14:11 -0300

    Fixed Invincibility Powerup,Spawn time added

--- asteroids/src/Game/Setup/Powerups.cpp
+++ asteroids/src/Game/Setup/Powerups.cpp
@@ -9,20 +9,38 @@ namespace Juego
 	namespace Gameplay_Section
 	{
 
+		static float powerupInvicibilitySpawnTimer = 0;
+		static int powerupInvicibilitySpawn = 15;
+
 		static float powerupInvincibilityTimer = 0;
-		static int powerupInvincibilityTimeCountdown = 3;
+		static int powerupInvincibilityTimeCountdown = 4;
 
 		void createPowerups()
 		{
 			powerupInvincibility.activated = false;
 			powerupInvincibility.onScreen = true;
-			powerupInvincibility.position = { (float)(GetRandomValue(0.0, (float)screenWidth)),(float)(GetRandomValue(0.0, (float)screenHeight)) };
+			powerupInvincibility.position = { (float)(GetRandomValue(0.0, screenWidth)),(float)(GetRandomValue(0.0, screenHeight)) };
 			powerupInvincibility.radius = 10;
 			powerupInvincibility.speed = {0,0};
 		}
 
 		void PowerupsUpdate()
 		{
+			if (!powerupInvincibility.onScreen)
+			{
+				powerupInvicibilitySpawnTimer += 1 * GetFrameTime();
+				if (powerupInvicibilitySpawnTimer > 1)
+				{
+					powerupInvicibilitySpawnTimer = 0;
+					powerupInvicibilitySpawn--;
+					if (powerupInvicibilitySpawn < 0)
+					{
+						powerupInvicibilitySpawn = 15;
+						powerupInvincibility.onScreen = true;
+					}
+				}
+			}
+
 			if (powerupInvincibility.activated)
 			{
 				powerupInvincibilityTimer += 1 * GetFrameTime();
@@ -37,19 +55,20 @@ namespace Juego
 
 			if (powerupInvincibilityTimeCountdown < 0)
 			{
-				powerupInvincibilityTimeCountdown = 10;
+				powerupInvincibilityTimeCountdown = 4;
 				player.textureTint = WHITE;
 				powerupInvincibility.activated = false;
-				powerupInvincibility.onScreen = true;
 			}
 
 			if (CheckCollisionCircles(powerupInvincibility.position, powerupInvincibility.radius, collisionCircle.position, collisionCircle.radius) && powerupInvincibility.onScreen)
 			{
+				powerupInvicibilitySpawn = 15;
 				powerupInvincibilityTimer = 0;
 				player.textureTint = RED;
 				PlaySound(powerup01);
 				powerupInvincibility.activated = true;
-				powerupInvincibility.onScreen = false;				
+				powerupInvincibility.onScreen = false;	
+				powerupInvincibility.position = { (float)(GetRandomValue(0.0, screenWidth)),(float)(GetRandomValue(0.0, screenHeight)) };
 			}
 		}
 
@@ -57,10 +76,10 @@ namespace Juego
 		{
 			if (powerupInvincibility.activated)
 			{
-				DrawText(FormatText("Powerup: %i", powerupInvincibilityTimeCountdown), screenWidth / 1.5, screenHeight / 14, 40, YELLOW);
+				DrawText(FormatText("Invincibility: %i", powerupInvincibilityTimeCountdown), screenWidth / 1.5, screenHeight / 14, 40, YELLOW);
 			}
 			
-			if (!powerupInvincibility.activated)
+			if (!powerupInvincibility.activated && powerupInvincibility.onScreen)
 			{
 				DrawCircleV(powerupInvincibility.position, powerupInvincibility.radius, RED);
 			}
